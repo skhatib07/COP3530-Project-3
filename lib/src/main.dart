330a1b6f2293addf64bd75e2127f306c804fdb6c
Samer Khatib <skhatib07@outlook.com>
2021-03-29 09:57:22 -0400

    Added first iteration of location services

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'locations.dart' as locations;
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: VaccinAppHome(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class VaccinAppHome extends StatefulWidget{
+  @override
+  _VaccinAppHomeState createState() => _VaccinAppHomeState();
+}
+
+class _VaccinAppHomeState extends State<VaccinAppHome> {
   final Map<String, Marker> _markers = {};
+  Location _location = Location();
   Future<void> _onMapCreated(GoogleMapController controller) async {
     final googleOffices = await locations.getGoogleOffices();
+    _location.onLocationChanged.listen((l) {
+      controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude!, l.longitude!),zoom: 15),
+        ),
+      );
+    });
     setState(() {
       _markers.clear();
       for (final office in googleOffices.offices) {
@@ -33,19 +52,20 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Office Locations'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: const LatLng(0, 0),
-            zoom: 2,
-          ),
-          markers: _markers.values.toSet(),
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(target: LatLng(20.5937, 78.9629)),
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+              markers: _markers.values.toSet(),
+            ),
+          ],
         ),
       ),
     );
